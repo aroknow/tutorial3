2898beffd2a840555707f12ec9ba23a2f1972822
Ramiro <aroknow@outlook.es>
2026-08-07 13:02:51 +1200

    Add second test (testSubtraction)

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -6,9 +6,13 @@ import org.junit.jupiter.api.Test;
 public class CalcTest {
   Calc c = new Calc();
 
-    @Test
-    void testAddition() {
-        assertEquals(4, c.add(2,2));
-    }
+  @Test
+  void testAddition() {
+    assertEquals(4, c.add(2,2));
+  }
 
+  @Test
+  void testSubtraction() {
+    assertEquals(2, c.subtract(4,2));
+  }
 }
